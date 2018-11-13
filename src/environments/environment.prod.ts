67171e08bfc2168232b535dd5e39e3ee5af44eb5
Jose Invernon <j.inver93@gmail.com>
2018-11-13 01:59:49 -0400

    Environment configured correctly for stackit2 and dev

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,12 +8,4 @@ export const environment = {
     storageBucket: "sistemas-de-informacion-unimet.appspot.com",
     messagingSenderId: "632212986753"
   }
-  // firebase : {
-  //   apiKey: "AIzaSyAaGASYnGTBhvWa0alghOVPO6Zc51gTa20",
-  //   authDomain: "stack-it-2.firebaseapp.com",
-  //   databaseURL: "https://stack-it-2.firebaseio.com",
-  //   projectId: "stack-it-2",
-  //   storageBucket: "stack-it-2.appspot.com",
-  //   messagingSenderId: "764145784673"
-  // }
 };
---
+++ src/environments/environment.stackit2.prod.ts
@@ -0,0 +1,19 @@
+export const environment = {
+  production: true,
+  // firebase : {
+  //   apiKey: "AIzaSyAQ5GeyxkEycrevO6yII5EB2B6rjZ8Cnrg",
+  //   authDomain: "sistemas-de-informacion-unimet.firebaseapp.com",
+  //   databaseURL: "https://sistemas-de-informacion-unimet.firebaseio.com",
+  //   projectId: "sistemas-de-informacion-unimet",
+  //   storageBucket: "sistemas-de-informacion-unimet.appspot.com",
+  //   messagingSenderId: "632212986753"
+  // }
+  firebase : {
+    apiKey: "AIzaSyAaGASYnGTBhvWa0alghOVPO6Zc51gTa20",
+    authDomain: "stack-it-2.firebaseapp.com",
+    databaseURL: "https://stack-it-2.firebaseio.com",
+    projectId: "stack-it-2",
+    storageBucket: "stack-it-2.appspot.com",
+    messagingSenderId: "764145784673"
+  }
+};
